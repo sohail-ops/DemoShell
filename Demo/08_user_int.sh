d9464da87eb58dcb68dc2ddc0d5abed6f1fa39b5
Sohail Patel <sohail.patel@neosoftmail.com>
2024-12-05 13:44:22 +0530

    changes made

--- Demo/08_user_int.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#taking input from user
-read -p "What is our name? " name
-echo "Your name is $name"
--- Demo/14_ternary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#condition1 && condition2 || condition3
-
-read -p "What is your age? " age
-[[ $age -ge 18 ]] && echo "Your are Adult" || echo "Minor"
